980f902a1c2d6ec9cd7f3e4b817d925380a4a6a7
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2025-05-01 13:28:13 +0500

    fix: dashboard design

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -6,7 +6,6 @@ import { notifyError, notifySuccess } from "../../helper/toast";
 
 export const Dashboard = () => {
   const navigate = useNavigate();
-
   const goSomewhere = () => {
     navigate("/dashboard");
   };
@@ -41,134 +40,149 @@ export const Dashboard = () => {
 
   return (
     <Box
-      onSubmit={handleSubmit}
-      component="form"
+      component="main"
       sx={{
+        minWidth: "100%",
         minHeight: "100vh",
-        minWidth: "500px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        textAlign: "center",
       }}
     >
-      <Typography variant="h3" margin={2}>
-        Creating book form
-      </Typography>
-      <TextField
-        label="Name"
-        name="name"
-        type="text"
-        value={bookData.name}
-        variant="outlined"
-        onChange={(e) => setBookData({ ...bookData, name: e.target.value })}
-        sx={{ width: 400, margin: 1 }}
-      />
+      <Box
+        onSubmit={handleSubmit}
+        component="form"
+        sx={{
+          minHeight: "50vh",
+          width: "500px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          border: "1px solid #494949",
+          borderRadius: "5px",
+          margin: "50px",
+        }}
+      >
+        <Typography variant="h3" margin={2} color="white">
+          Creating book form
+        </Typography>
+        <TextField
+          label="Name"
+          name="name"
+          type="text"
+          value={bookData.name}
+          variant="outlined"
+          onChange={(e) => setBookData({ ...bookData, name: e.target.value })}
+          sx={{ width: 400, margin: 1 }}
+        />
 
-      <TextField
-        label="Book number"
-        name="bookName"
-        type="number"
-        value={bookData.bookNumber}
-        variant="outlined"
-        onChange={(e) =>
-          setBookData({ ...bookData, bookNumber: e.target.value })
-        }
-        sx={{ width: 400, margin: 1 }}
-      />
+        <TextField
+          label="Book number"
+          name="bookName"
+          type="number"
+          value={bookData.bookNumber}
+          variant="outlined"
+          onChange={(e) =>
+            setBookData({ ...bookData, bookNumber: e.target.value })
+          }
+          sx={{ width: 400, margin: 1 }}
+        />
 
-      <TextField
-        label="Book type"
-        name="bookType"
-        type="text"
-        value={bookData.bookType}
-        variant="outlined"
-        onChange={(e) => setBookData({ ...bookData, bookType: e.target.value })}
-        sx={{ width: 400, margin: 1 }}
-      />
+        <TextField
+          label="Book type"
+          name="bookType"
+          type="text"
+          value={bookData.bookType}
+          variant="outlined"
+          onChange={(e) =>
+            setBookData({ ...bookData, bookType: e.target.value })
+          }
+          sx={{ width: 400, margin: 1 }}
+        />
 
-      <TextField
-        label="Language type"
-        name="lanType"
-        type="text"
-        value={bookData.languageType}
-        variant="outlined"
-        onChange={(e) =>
-          setBookData({ ...bookData, languageType: e.target.value })
-        }
-        sx={{ width: 400, margin: 1 }}
-      />
+        <TextField
+          label="Language type"
+          name="lanType"
+          type="text"
+          value={bookData.languageType}
+          variant="outlined"
+          onChange={(e) =>
+            setBookData({ ...bookData, languageType: e.target.value })
+          }
+          sx={{ width: 400, margin: 1 }}
+        />
 
-      <TextField
-        label="Created year"
-        name="createdYear"
-        type="number"
-        value={bookData.createdYear}
-        variant="outlined"
-        onChange={(e) =>
-          setBookData({ ...bookData, createdYear: e.target.value })
-        }
-        sx={{ width: 400, margin: 1 }}
-      />
+        <TextField
+          label="Created year"
+          name="createdYear"
+          type="number"
+          value={bookData.createdYear}
+          variant="outlined"
+          onChange={(e) =>
+            setBookData({ ...bookData, createdYear: e.target.value })
+          }
+          sx={{ width: 400, margin: 1 }}
+        />
 
-      <TextField
-        label="Made by"
-        name="madeBy"
-        type="text"
-        value={bookData.madeBy}
-        variant="outlined"
-        onChange={(e) => setBookData({ ...bookData, madeBy: e.target.value })}
-        sx={{ width: 400, margin: 1 }}
-      />
+        <TextField
+          label="Made by"
+          name="madeBy"
+          type="text"
+          value={bookData.madeBy}
+          variant="outlined"
+          onChange={(e) => setBookData({ ...bookData, madeBy: e.target.value })}
+          sx={{ width: 400, margin: 1 }}
+        />
 
-      <TextField
-        label="ISBN"
-        name="isbn"
-        type="text"
-        value={bookData.isbn}
-        variant="outlined"
-        onChange={(e) => setBookData({ ...bookData, isbn: e.target.value })}
-        sx={{ width: 400, margin: 1 }}
-      />
+        <TextField
+          label="ISBN"
+          name="isbn"
+          type="text"
+          value={bookData.isbn}
+          variant="outlined"
+          onChange={(e) => setBookData({ ...bookData, isbn: e.target.value })}
+          sx={{ width: 400, margin: 1 }}
+        />
 
-      <TextField
-        label="Book page"
-        name="bookPage"
-        type="number"
-        value={bookData.bookPage}
-        variant="outlined"
-        onChange={(e) => setBookData({ ...bookData, bookPage: e.target.value })}
-        sx={{ width: 400, margin: 1 }}
-      />
+        <TextField
+          label="Book page"
+          name="bookPage"
+          type="number"
+          value={bookData.bookPage}
+          variant="outlined"
+          onChange={(e) =>
+            setBookData({ ...bookData, bookPage: e.target.value })
+          }
+          sx={{ width: 400, margin: 1 }}
+        />
 
-      <TextField
-        label="Digitization date"
-        name="digitizationDate"
-        type="date"
-        value={bookData.digitizationDate}
-        variant="outlined"
-        onChange={(e) =>
-          setBookData({ ...bookData, digitizationDate: e.target.value })
-        }
-        sx={{ width: 400, margin: 1 }}
-      />
+        <TextField
+          label="Digitization date"
+          name="digitizationDate"
+          type="date"
+          value={bookData.digitizationDate}
+          variant="outlined"
+          onChange={(e) =>
+            setBookData({ ...bookData, digitizationDate: e.target.value })
+          }
+          sx={{ width: 400, margin: 1 }}
+        />
 
-      <TextField
-        label="Digitization by"
-        name="digitizationBy"
-        type="text"
-        value={bookData.digitizationBy}
-        variant="outlined"
-        onChange={(e) =>
-          setBookData({ ...bookData, digitizationBy: e.target.value })
-        }
-        sx={{ width: 400, margin: 1 }}
-      />
+        <TextField
+          label="Digitization by"
+          name="digitizationBy"
+          type="text"
+          value={bookData.digitizationBy}
+          variant="outlined"
+          onChange={(e) =>
+            setBookData({ ...bookData, digitizationBy: e.target.value })
+          }
+          sx={{ width: 400, margin: 1 }}
+        />
 
-      <Button type="submit" variant="contained" sx={{ margin: "20px" }}>
-        Submit
-      </Button>
+        <Button type="submit" variant="contained" sx={{ margin: "20px" }}>
+          Submit
+        </Button>
+      </Box>
     </Box>
   );
 };
